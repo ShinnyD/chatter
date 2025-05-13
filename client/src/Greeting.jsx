b40764b27a91f886deaa491f51fdea958783cdb6
shinobuDuerloo <shinobu.duerloo@corporatemontage.de>
2025-05-13 21:57:43 +0200

    adds greeting container

---
+++ client/src/Greeting.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Greeting() {
+  return (
+    <div className="space-y-2">
+      <h1 className="text-3xl font-bold text-green-600">👋 Welcome!</h1>
+      <p className="text-lg text-gray-700">Which language would you like to practice speaking?</p>
+    </div>
+  );
+}
