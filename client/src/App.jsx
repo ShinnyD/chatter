0a9b9883f53afe5a4e52db7175d6ee84705bc237
shinobuDuerloo <shinobu.duerloo@corporatemontage.de>
2025-05-13 21:58:39 +0200

    adjusts boiler plate frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
+import LanguageSelector from './LanguageSelector';
 
 function App() {
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    fetch('/api')
-      .then((res) => res.text())
-      .then((data) => setMessage(data))
-      .catch((err) => console.error("Fetch error:", err));
-  }, []);
-
-  return (
-    <div className="App">
-      <h1>Frontend connected to Backend</h1>
-      <p>{message}</p>
-    </div>
-  );
+  return <LanguageSelector />;
 }
 
 export default App;
